a7a3d6c666b0c499ad089c5dd13bdfcf456d3e81
raehan <raehan900809@gmail.com>
2022-11-08 14:33:56 +0900

    modify: backend url

--- front/config/config.js
+++ front/config/config.js
@@ -1,1 +1,2 @@
-export const backUrl = process.env.NODE_ENV === 'production' ? '' : process.env.NEXT_PUBLIC_BACK_URL;
+// export const backUrl = process.env.NODE_ENV === 'production' ? '' : process.env.NEXT_PUBLIC_BACK_URL;
+export const backUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BACK_URL : process.env.NEXT_PUBLIC_BACK_URL;
--- front/pages/m/[storeId].js
+++ front/pages/m/[storeId].js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {useRouter} from "next/router";
 import wrapper from "../../store/configureStore";
 import DefaultLayout from "../../layouts/DefaultLayout";
