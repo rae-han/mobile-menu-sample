a12acb7e980f88618b86971bf413251aa7b66da7
raehan <raehan900809@gmail.com>
2022-11-25 19:00:29 +0900

    add: lambda for file upload fix: API gateway for binary(image buffer)

--- lambda/imageUpload/index.js
+++ lambda/imageUpload/index.js
@@ -1,20 +1,24 @@
-const { Readable } = require('stream');
-const AWS = require('aws-sdk');
-const multer = require('multer');
-const sharp = require('sharp');
+// const { Readable } = require('stream');
+// const multer = require('multer');
+// const sharp = require('sharp');
 
 exports.handler = async (event, context, callback) => {
-    console.log('######## ######## event #### ');
-    console.log(event);
-    console.log(`######## ######## context #### `);
+    console.log('######## ######## callback ########');
+    console.log('callback', callback);
+    console.log('######## ######## context ########');
     console.log(context);
-    console.log(`######## ######## callback ####`);
-    console.log(callback)
+    console.log('######## ######## event ########');
+    console.log(event);
+
+    console.log('######## ######## logic ########');
+    console.log(event.body)
+    console.log(JSON.parse(event.body));
 
     // TODO implement
+
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        // body: JSON.stringify('Hello from Lambda!'),
     };
     return response;
 };
