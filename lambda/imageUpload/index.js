846d3a9aa0fe3541398ed8dc20022ef14267e874
raehan <raehan900809@gmail.com>
2022-11-25 19:00:54 +0900

    modify: lambda logger

--- lambda/imageUpload/index.js
+++ lambda/imageUpload/index.js
@@ -3,22 +3,21 @@
 // const sharp = require('sharp');
 
 exports.handler = async (event, context, callback) => {
-    console.log('######## ######## callback ########');
     console.log('callback', callback);
-    console.log('######## ######## context ########');
-    console.log(context);
-    console.log('######## ######## event ########');
-    console.log(event);
+    console.log('context', context);
+    console.log('event', event);
 
-    console.log('######## ######## logic ########');
-    console.log(event.body)
-    console.log(JSON.parse(event.body));
+    console.log('event.body', event.body);
+    const body = event.body;
+    console.log('body', body);
+
+    const bodyBuffer = Buffer.from(body.toString(), "base64")
+
 
     // TODO implement
-
     const response = {
         statusCode: 200,
-        // body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify(bodyBuffer),
     };
     return response;
 };
