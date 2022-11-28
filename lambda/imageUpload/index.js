1cb5c7c72b0fb3e295feb7aca5db68750d49492b
raehan <raehan900809@gmail.com>
2022-11-28 16:50:25 +0900

    add: lambda multipart parser

--- lambda/imageUpload/index.js
+++ lambda/imageUpload/index.js
@@ -1,6 +1,7 @@
 // const { Readable } = require('stream');
 // const multer = require('multer');
 // const sharp = require('sharp');
+const parser = require('lambda-multipart-parser');
 
 exports.handler = async (event, context, callback) => {
     console.log('callback', callback);
@@ -8,16 +9,35 @@ exports.handler = async (event, context, callback) => {
     console.log('event', event);
 
     console.log('event.body', event.body);
-    const body = event.body;
-    console.log('body', body);
+    // const body = event.body;
+    // console.log('body', body);
+    //
+    // const bodyBuffer = Buffer.from(body, "base64").toString("utf8")
+    // const contentType = event.headers["Content-Type"] || event.headers["content-type"];
+    //
+    // const contentTypes = contentType.split(';');
+    // let boundary = '';
+    //
+    // if (contentTypes) {
+    //     const boundaryString = contentTypes.find((item) => item.trim().includes('boundary'))
+    //     boundary = boundaryString.trim().split('=')[1];
+    // }
 
-    const bodyBuffer = Buffer.from(body, "base64").toString("utf8")
-    const boundary = event.headers["Content-Type"]
+
+    const result = await parser.parse(event);
+    const files = result.files;
+
+    console.log(files);
 
     // TODO implement
     const response = {
+        headers: {
+            "Content-Type": "application/json"
+        },
         statusCode: 200,
-        body: JSON.parse(JSON.stringify(boundary)),
+        // body: JSON.parse(JSON.stringify(files)),
+        body: 123,
     };
+
     return response;
 };
